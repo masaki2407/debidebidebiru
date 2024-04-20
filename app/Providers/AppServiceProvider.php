3eb84491de213ac73fc9eb1a53f12fc06120945a
ItsukiKoido <af22080@shibaura-it.ac.jp>
2024-04-20 16:44:56 +0900

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        \URL::forceScheme('https');
+        
+        //ページネーション機能
+        \URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
